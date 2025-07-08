b4532a7d61848f12fedbe5d38b85156a59ae9058
Alex Santos <alexsantosquispe@gmail.com>
2025-07-07 22:26:42 -0400

    eslint and prettier setup

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,23 +1,40 @@
-import { globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier'; // Import the prettier plugin
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default defineConfig([
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite
-    ],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021
+      }
+    },
+    extends: [
+      js.configs.recommended, // Equivalent to "eslint:recommended"
+      ...tseslint.configs.recommended, // If you're using TypeScript
+      pluginReact.configs.recommended, // Equivalent to "plugin:react/recommended"
+      prettierRecommended // Equivalent to "plugin:prettier/recommended"
+    ],
+    plugins: {
+      react: pluginReact, // Define react plugin
+      prettier: prettier // Define prettier plugin
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // Your custom rule
+      'prettier/prettier': 'error' // Your custom rule
+    },
+    settings: {
+      react: {
+        version: 'detect' // Equivalent to "version": "detect"
+      }
     }
   }
 ]);
