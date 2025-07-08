d72fbd821e84cb8235a1cf933a2c69a1f82f50b6
Alex Santos <alexsantosquispe@gmail.com>
2025-07-07 23:02:43 -0400

    eslint setup updated

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,40 +1,41 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactPlugin from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import js from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
-import prettier from 'eslint-plugin-prettier'; // Import the prettier plugin
-import prettierRecommended from 'eslint-plugin-prettier/recommended';
-import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.es2021
-      }
-    },
-    extends: [
-      js.configs.recommended, // Equivalent to "eslint:recommended"
-      ...tseslint.configs.recommended, // If you're using TypeScript
-      pluginReact.configs.recommended, // Equivalent to "plugin:react/recommended"
-      prettierRecommended // Equivalent to "plugin:prettier/recommended"
-    ],
-    plugins: {
-      react: pluginReact, // Define react plugin
-      prettier: prettier // Define prettier plugin
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off', // Your custom rule
-      'prettier/prettier': 'error' // Your custom rule
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: globals.browser
     },
     settings: {
       react: {
-        version: 'detect' // Equivalent to "version": "detect"
+        version: 'detect'
       }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error'
     }
   }
 ]);
